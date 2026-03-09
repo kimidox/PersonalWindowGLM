4c21dadb45868fa3f2266e4203b68224fc919144
kimido <kimido101@163.com>
2026-03-09 18:00:47 +0800

    init1.3v

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ def get_config(key:str):
     import dotenv
 
     dotenv.load_dotenv()
-    return dotenv.get_key(dotenv_path=".env",key_to_get=key)
+    return dotenv.get_key(dotenv_path=".env.dev",key_to_get=key)
 
 OPENAI_API_KEY = get_config("OPENAI_API_KEY")
 OPENAI_BASE_URL = get_config("OPENAI_BASE_URL")
